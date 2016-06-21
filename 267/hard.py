7f88313e5490774409491ddfe68203f11ccd2dfe
Daniel Bratsberg <daniel@bratsberg.co.uk>
2016-06-21 22:17:32 +0200

    (#267): Added grain, and very simple performance stats.

--- 267/hard.py
+++ 267/hard.py
@@ -2,6 +2,7 @@
 
 size = (0, 0)
 zombies = list()
+checks = 0
 
 def importFromFile(filename):
     global size
@@ -30,6 +31,8 @@ def importFromFile(filename):
     # End Debug
 
 def _hit(shot, pos):
+    global checks
+    checks += 1
     if int(shot[0]) >= int(pos[0]) - 1 and \
        int(shot[0]) <= int(pos[0]) + 1 and \
        int(shot[1]) >= int(pos[1]) - 1 and \
@@ -50,25 +53,40 @@ def findOptimalShot():
     count = 0
     lrDiaCount = 0
     rlDiaCount = 0
+
     # Search vertically
     print ("For in range... " + str(size) + " : " + str(len(zombies)))
 
     # TODO: Optimise this, please
     # Horizontally
-
+    grain = 3
+    loops = 0
     print ("horz")
-    for x in range (0, int(size[0])):
+    for x in range (0, int(size[0]), grain):
+        loops += 1
         # New shot
         count = 0
         shotZombies = []
         testedCoords = []
 
         for y in range(0, int(size[1])):
+            loops += 1
             testedCoords.append((x, y))
             for z in zombies:
+                loops += 1
                 if _hit(z, (x, y)) and not z in shotZombies:
                     count += 1
                     shotZombies.append(z)
+                    # belongs[x, y, v] -> v
+                    # belongs[x, y, h] -> h
+                    # belongs[x, y, d] -> d
+                    # belongs[x, y, r] -> r
+                    # 0, 0 -> 0
+                    # 1, 1 -> 0
+                    # 1, 2 -> -1 = Intersects with Y (1, 2)
+                    # 2, 1 -> 1 = Inter with X (2, 1)
+                    # 31, 24 -> 7 inter with X (7, 1)
+                    # 42, 84 -> -42 inter with Y at 42. (1, 42)
             if count > bestShotKills:
                 bestShotKills = count
                 bestShotDir = (x, 0)
@@ -76,15 +94,18 @@ def findOptimalShot():
 
     # Vertically
     print ("vert")
-    for y in range (0, int(size[1])):
+    for y in range (0, int(size[1]), grain):
+        loops += 1
         # New shot
         count = 0
         shotZombies = []
         testedCoords = []
 
         for x in range(0, int(size[0])):
+            loops += 1
             testedCoords.append((x, y))
             for z in zombies:
+                loops += 1
                 if _hit(z, (x, y)) and not z in shotZombies:
                     count += 1
                     shotZombies.append(z)
@@ -105,7 +126,8 @@ def findOptimalShot():
     '''
 
     print ("dlr")
-    for col in range (0, int(size[0])):
+    for col in range (0, int(size[0]), grain):
+        loops += 1
         # New shot
         count = 0
         shotZombies = []
@@ -114,9 +136,11 @@ def findOptimalShot():
         # Don't use double for...
         y = 0
         for x in range (col, int(size[0])):
+            loops += 1
             testedCoords.append((x, y))
             #for y in range(0, int(size[1])):
             for z in zombies:
+                loops += 1
                 if _hit(z, (x, y)) and not z in shotZombies:
                     count += 1
                     shotZombies.append(z)
@@ -129,7 +153,8 @@ def findOptimalShot():
                 break
 
     print("drl")
-    for col in range (int(size[0]) - 1, -1, -1):
+    for col in range (int(size[0]) - 1, -1, grain * -1):
+        loops += 1
         # New shot
         count = 0
         shotZombies = []
@@ -138,9 +163,11 @@ def findOptimalShot():
         # Don't use double for...
         y = 0
         for x in range (col, -1, -1):
+            loops += 1
             testedCoords.append((x, y))
             #for y in range(0, int(size[1])):
             for z in zombies:
+                loops += 1
                 if _hit(z, (x, y)) and not z in shotZombies:
                     #print (str(x) + ":" + str(y) + " killed " + str(z))
                     count += 1
@@ -169,8 +196,10 @@ def findOptimalShot():
 
 
     print ("BestShotKills: " + str(bestShotKills) + ", BestShotDirection: " + str(bestShotDir[0]) + ":" + str(bestShotDir[1]))
+    print ("Loops: " + str(loops))
 
 
 if __name__ == '__main__':
     importFromFile("./input")
     findOptimalShot()
+    print ("Number of checks: " + str(checks))
